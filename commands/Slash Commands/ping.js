2445bffc5c879323220bb14ca51fb0e8d8e51409
Kazuki <136700294+HanatoKazuki@users.noreply.github.com>
2024-01-06 19:19:01 +0800

    Create ping.js

---
+++ commands/Slash Commands/ping.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: "ping",
+  category: "Utilities",
+  description: "Slash ping command.",
+  type: "interaction",
+  prototype: "slash",
+  code: `$interactionReply[$ping ms;;;;everyone;true]`
+}
