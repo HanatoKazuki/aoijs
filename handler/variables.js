46720764ee575214fc1a06094c00a23574083750
Kazuki <136700294+HanatoKazuki@users.noreply.github.com>
2024-01-07 20:55:20 +0800

    Update variables.js

--- handler/variables.js
+++ handler/variables.js
@@ -1,6 +1,13 @@
 module.exports = (client) => {
   client.variables({
     cash: 0,
-    bank: 0
+    bank: 0,
+    level: 0,
+    exp: 0,
+    maxexp: 0,
+    welcomer: false,
+    welcomerchannel: "0000",
+    welcomermessage: "Welcome To The Guild, {user.mention}!",
+    welcomerembedsmessage: {"title": "{guild.name}", "description": "Welcome To The Server, {user.mention}!", "color": "White", "image": ""}
   }, "main"); // The label "main" here is the table of the variable, do not change it unless you're sure what you're doing.
 };
