109d75ec85be2d02bae08cf7e650506b44db73ae
Kazuki <136700294+HanatoKazuki@users.noreply.github.com>
2024-01-07 13:05:09 +0800

    Update ping.js

--- commands/Slash Commands/ping.js
+++ commands/Slash Commands/ping.js
@@ -6,3 +6,5 @@ module.exports = {
   prototype: "slash",
   code: `$interactionReply[$ping ms;;;;everyone;true]`
 }
+// Using the functions $interactionReply would reply to your message in epehemeral, in order to make it visible to others again change the last parameter from "true" to "false" to make it visible to others again.
+// Please take note that $interactionReply function would only useable if the type of commands was buttons, select menu or slashes.
