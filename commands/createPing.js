52e786328e049dc74f9e257dbc0c0235f194ed11
Kazuki <136700294+HanatoKazuki@users.noreply.github.com>
2024-01-07 13:02:31 +0800

    Update and rename commands/Normal Commands/multipleSlashCommand.js to commands/createPing.js

--- commands/createPing.js
+++ commands/createPing.js
@@ -1,4 +1,4 @@
-module.exports = [{
+module.exports = {
   name: "createPing",
   description: "Create a slash ping slash command.",
   category: "Setup",
@@ -6,4 +6,6 @@ module.exports = [{
   Slash "Ping" command have been created successfully!
   $createApplicationCommand[$guildID;ping;Display ping ms;true;slash]
   $onlyForIDs[$clientOwnerIDs[;];]`
-}]
+}
+// The first parameter of $createApplicationCommand function can be changes to either to a specify guild id or as a global.
+// Using $onlyForIDs function will return the error message if the user's id was not match to the specific IDs.
